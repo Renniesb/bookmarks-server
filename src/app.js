d77773342625a95c5458e3c2aad367b9951e67cb
Rennie Bevineau <rsbevineau@gmail.com>
2020-10-29 19:35:45 -0400

    add api route

--- src/app.js
+++ src/app.js
@@ -3,19 +3,29 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-
+const apiRouter = require('./routes/api')
 const app = express()
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
-  skip: () => NODE_ENV === 'test',
-}))
+app.use(morgan('dev'))
 app.use(cors())
 app.use(helmet())
+
+app.use(function validateBearerToken(req, res, next){
+  const authToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN
+  if(!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({error: 'Unauthorized Request'})
+  }
+
+  next()
+
+})
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
+app.use('api/', apiRouter)
 app.use(function errorHandler(error, req, res, next) {
   let response
   if (NODE_ENV === 'production') {
---
+++ src/routes/api.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+//route handler for GET /bookmarks
+router.get('/bookmarks',(req, res)=>{
+
+})
