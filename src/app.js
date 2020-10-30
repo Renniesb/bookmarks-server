6f7ec9006fe8ca7d5d426fc8cff6f8e29f027f4d
Rennie Bevineau <rsbevineau@gmail.com>
2020-10-29 20:13:55 -0400

    set up logger and error handling

--- src/app.js
+++ src/app.js
@@ -1,40 +1,30 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
 const apiRouter = require('./routes/api')
+
 const app = express()
 
-app.use(morgan('dev'))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
 app.use(helmet())
+app.use(validateBearerToken)
 
-app.use(function validateBearerToken(req, res, next){
-  const authToken = req.get('Authorization')
-  const apiToken = process.env.API_TOKEN
-  if(!authToken || authToken.split(' ')[1] !== apiToken) {
-      return res.status(401).json({error: 'Unauthorized Request'})
-  }
 
-  next()
-
-})
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
 app.use('api/', apiRouter)
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: 'server error' }
-  } else {
-    console.error(error)
-    response = { error: error.message, details: error }
-  }
-  res.status(500).json(response)
-})
+
+app.use(errorHandler);
 
 module.exports = app
